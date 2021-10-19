542143c8507e8f639f0bae97f5f83c6577e6018e
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2021-10-19 15:34:41 +0200

    update

--- Algorithms/policy.py
+++ Algorithms/policy.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 class EpsilonGreedyPolicy(object):
     """
     A simple epsilon greedy policy.
     """
-    def __init__(self, epsilon, Q_a : dict, Q_b: dict = None):
+
+    def __init__(self, epsilon, Q_a: dict, Q_b: dict = None):
         self.Q_a = Q_a
         self.Q_b = Q_b
         self.epsilon = epsilon
@@ -19,6 +21,8 @@ class EpsilonGreedyPolicy(object):
         Returns:
             An action (int).
         """
+
+        print(f"Q_a = {self.Q_a}")
         if self.Q_b:
             action_values = self.Q_a[obs] + self.Q_b[obs]
         else:
@@ -28,5 +32,5 @@ class EpsilonGreedyPolicy(object):
             action = np.random.randint(0, len(action_values))
         else:
             action = np.argmax(action_values)
-                        
+
         return action
